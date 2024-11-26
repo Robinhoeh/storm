54f3419c796192399d6ff34a95900554098883b7
robinwatson <drumsrobin@gmail.com>
2024-11-26 12:16:48 -0500

    add normalize

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <h1>Hello</h1>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
   </header>
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
 
-<style scoped>
-header {
+<style scoped lang="scss">
+/* header {
   line-height: 1.5;
 }
 
@@ -43,5 +43,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
